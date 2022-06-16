9b6130ba758d4ffc61ab0a27af70bc2df2df711c
RealTheDev <93186778+RealTheDev@users.noreply.github.com>
2022-06-17 00:01:52 +0300

    Update script.js

--- CoolAwesomeGreatWebsite/script.js
+++ CoolAwesomeGreatWebsite/script.js
@@ -1,13 +1,33 @@
 const chk = document.getElementById('chk');
+const loadedSettings = JSON.parse(localStorage.getItem('settings'));
 
 chk.addEventListener('change', () => {
-document.body.classList.toggle('dark');
-document.getElementById('toggle').classList.toggle('dark');
-document.getElementById('ball').classList.toggle('dark');
+    var settings = {
+        darkmode: chk.checked
+    };
+    localStorage.setItem('settings', JSON.stringify(settings));
+    toggleDark();
 });
 
 if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.toggle('dark');
-    document.getElementById('toggle').classList.toggle('dark');
-    document.getElementById('ball').classList.toggle('dark');
+    if (loadedSettings.darkmode != null && loadedSettings.darkmode != false){
+        chk.checked = true
+        var settings = {
+            darkmode: chk.checked
+        };
+        localStorage.setItem('settings', JSON.stringify(settings));
+        toggleDark();
+    }
 };
+
+function toggleDark() {
+    if(JSON.parse(localStorage.getItem('settings')).darkmode){
+         document.body.classList.add('dark');
+        document.getElementById('toggle').classList.add('dark');
+        document.getElementById('ball').classList.add('dark');
+        } else{
+        document.body.classList.remove('dark');
+        document.getElementById('toggle').classList.remove('dark');
+        document.getElementById('ball').classList.remove('dark');
+        }
+}
