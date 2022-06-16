1d44a76026e0a49cc080d0a287fda7037a10df44
RealTheDev <93186778+RealTheDev@users.noreply.github.com>
2022-06-16 17:07:56 +0300

    Update script.js

--- CoolAwesomeGreatWebsite/script.js
+++ CoolAwesomeGreatWebsite/script.js
@@ -5,3 +5,9 @@ document.body.classList.toggle('dark');
 document.getElementById('toggle').classList.toggle('dark');
 document.getElementById('ball').classList.toggle('dark');
 });
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.toggle('dark');
+    document.getElementById('toggle').classList.toggle('dark');
+    document.getElementById('ball').classList.toggle('dark');
+};
